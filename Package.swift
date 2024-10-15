2354e5911da9a412225865de9b999cbb4266f723
yiliangwang <yiliangwang@tencent.com>
2024-10-15 16:47:22 +0800

    package

--- Package.swift
+++ Package.swift
@@ -17,10 +17,7 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(name: "TUICore",
-                path: "TUICore",
-                resources: [.copy("Resources")]
-               )
+        .target(name: "TUICore")
         
     ]
 )
