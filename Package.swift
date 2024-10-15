3ca7f64584e2dc8ed90657ac38753bf33eb959b1
yiliangwang <yiliangwang@tencent.com>
2024-10-15 16:04:52 +0800

    package

--- Package.swift
+++ Package.swift
@@ -9,12 +9,12 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "TUICore",
-            targets: ["TUICore"]),
+            targets: ["TUICoreZip"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .binaryTarget(name: "TUICore", 
+        .binaryTarget(name: "TUICoreZip",
                       url: "https://im.sdk.cloud.tencent.cn/download/tuikit/8.2.6325/ios/TUICore.zip",
                       checksum: "37382423e17783c04d4d248b90fcc239f1290c26027925797c7f428effa9b2c9")
     ]
